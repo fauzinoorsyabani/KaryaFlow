2aa71fed0e2495b6e16a3f7a56d923dcca46d70d
FauziNs <227007042@student.unsil.ac.id>
2025-02-07 17:44:28 +0700

    Menambahkan file baru

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,17 +8,16 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
-              Video Editing
+              Welcome to IT Solution Tasikmalaya
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly simple
+              Solusi IT Terbaik
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
-              We designed XORA AI Video Editor to be an easy to use, quick to
-              learn, and surprisingly powerful.
+              Kami melayani jasa pembuatan website, aplikasi mobile, dan jasa IT
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
-              <Button icon="/images/zap.svg">Try it now</Button>
+              <Button icon="/images/zap.svg">Booking Sekarang Juga</Button>
             </LinkScroll>
           </div>
 
